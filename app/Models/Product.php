6ae417ee23a88ecd6a7d6d901ea91b607ca196b3
shalvah <diakon.ng@gmail.com>
2018-04-16 21:44:01 +0100

    Audit Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Product extends Model
+class Product extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+
     public const CATEGORIES = [
         'Phones and Electronics',
         'Fashion',
@@ -14,4 +17,5 @@ class Product extends Model
     ];
 
     protected $guarded = [];
+
 }
